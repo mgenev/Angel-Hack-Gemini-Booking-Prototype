9ee17ae0a0b07c6d0cf77e620226f0b640f5ee6e
mgenev <mgenev@gmail.com>
2013-11-02 13:59:21 -0700

    clients model  and controller added

---
+++ app/controllers/clients.js
@@ -0,0 +1,91 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    async = require('async'),
+    Article = mongoose.model('Client'),
+    _ = require('underscore');
+
+
+/**
+ * Find article by id
+ */
+exports.client = function(req, res, next, id) {
+    Client.load(id, function(err, client) {
+        if (err) return next(err);
+        if (!client) return next(new Error('Failed to load client ' + id));
+        req.client = client;
+        next();
+    });
+};
+
+/**
+ * Create a article
+ */
+exports.create = function(req, res) {
+    var client = new Client(req.body);
+    client.user = req.user;
+
+    client.save(function(err) {
+        if (err) {
+            return res.send('users/signup', {
+                errors: err.errors,
+                client: client
+            });
+        } else {
+            res.jsonp(client);
+        }
+    });
+};
+
+/**
+ * Update a article
+ */
+exports.update = function(req, res) {
+    var client = req.client;
+
+    client = _.extend(client, req.body);
+
+    client.save(function(err) {
+        res.jsonp(client);
+    });
+};
+
+/**
+ * Delete an article
+ */
+exports.destroy = function(req, res) {
+    var client = req.client;
+
+    client.remove(function(err) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(client);
+        }
+    });
+};
+
+/**
+ * Show an article
+ */
+exports.show = function(req, res) {
+    res.jsonp(req.client);
+};
+
+/**
+ * List of Articles
+ */
+exports.all = function(req, res) {
+    Client.find().sort('-created').populate('user', 'name username').exec(function(err, clients) {
+        if (err) {
+            res.render('error', {
+                status: 500
+            });
+        } else {
+            res.jsonp(clients);
+        }
+    });
+};
---
+++ app/models/client.js
@@ -0,0 +1,73 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    config = require('../../config/config'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Article Schema
+ */
+var ClientSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    address: {
+        city: {
+            type: String,
+            default: '',
+            trim: true
+        },
+        country: {
+            type: String,
+            default: '',
+            trim: true
+        },
+        street: {
+            type: String,
+            default: '',
+            trim: true
+        },
+        zip: {
+            type: String,
+            default: '',
+            trim: true
+        },
+    },
+    type: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    // user: {
+    //     type: Schema.ObjectId,
+    //     ref: 'User'
+    // }
+});
+
+/**
+ * Validations
+ */
+ClientSchema.path('title').validate(function(title) {
+    return title.length;
+}, 'Title cannot be blank');
+
+/**
+ * Statics
+ */
+// ClientSchema.statics = {
+//     load: function(id, cb) {
+//         this.findOne({
+//             _id: id
+//         }).populate('user', 'name username').exec(cb);
+//     }
+// };
+
+mongoose.model('Client', ClientSchema);
